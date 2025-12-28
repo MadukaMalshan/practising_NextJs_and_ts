4794ca6e987d42f3e5057af8bfc1af7c7a866748
MadukaMalshan <malshanmaduka8@gmail.com>
2025-12-29 01:53:25 +0530

    Fix some issues

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,13 @@
 import Input from "@/components/Input";
+import Button from "@/components/Button";
 
 export default function LoginPage() {
   return (
     <div className="max-w-sm mx-auto mt-20 space-y-4">
+      <h1 className="text-2xl font-bold text-slate-900">Login</h1>
       <Input placeholder="Email" />
       <Input type="password" placeholder="Password" />
+      <Button href="/" text="Sign In"/>
     </div>
   );
 }
